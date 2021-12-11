5b2541c0a86ef6688eda00d1feb7a5b611f6b56d
Ryan Moses <ryanchristophermoses@gmail.com>
2021-12-11 08:28:15 -0500

    Update our menu-items to navigate with HOC -HOC -- Higher Order Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
-import HomePage from './pages/homepage/homepage.component'
+import HomePage from './pages/homepage/homepage.component';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
+
 function App() {
-  return <div>
-    <HomePage />
-  </div>;
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
