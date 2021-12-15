cdc90016346df5f55c19a05d1cb1851da54d39aa
Ryan Moses <ryanchristophermoses@gmail.com>
2021-12-15 07:48:16 -0500

    Updates Webpage to have forms

--- src/App.js
+++ src/App.js
@@ -3,13 +3,9 @@ import HomePage from './pages/homepage/homepage.component';
 import { Route, Switch } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component'
 import Header from './components/header/header.component'
+import SignInAndSignUpPage from './pages/sign-in-out/sign-in-out.component';
 import './App.css';
 
-// const HatsPage = () => (
-//   <div>
-//     <h1>HATS PAGE</h1>
-//   </div>
-// );
 
 function App() {
   return (
@@ -18,6 +14,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -6,18 +6,24 @@ import './header.styles.scss';
 const Header = () => (
   <div className="header">
     <Link className="logo-container" to="/">
-        <Logo className='logo' />
+      <Logo className="logo" />
     </Link>
-    <div className='options'>
-        <Link className='option' to='/shop'>
-            SHOP
-        </Link>
-        <Link className='option' to='/shop'>
-            CONTACT
-        </Link>
+    <div className="options">
+      <Link className="option" to="/shop">
+        SHOP
+      </Link>
+      <Link className="option" to="/shop">
+        CONTACT
+      </Link>
     </div>
   </div>
-
 );
 
 export default Header;
+
+// In this lesson, you may have seen the strange syntax: import { ReactComponent as Logo }
+// This is a new special syntax when importing SVG in React. 
+// The ReactComponent import name is special and tells Create React App that you want a React component that renders an SVG, 
+// rather than its filename. You can read more about it here, but keep in mind that this is a React library special syntax:
+
+// https://facebook.github.io/create-react-app/docs/adding-images-fonts-and-files
